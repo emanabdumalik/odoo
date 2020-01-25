160b0ce2de14418453995f6afa0f1094c46eda01
semirgoldu <semirworkuaman@gmail.com>
2020-01-25 23:57:14 +0300

    Man

--- point_of_sale_categ_filter/__manifest__.py
+++ point_of_sale_categ_filter/__manifest__.py
@@ -7,10 +7,14 @@
 	"version" : "1.0.0.0",
     "author": "Eman",
     "license":"LGPL-3",
-    'category':'Point of Sale',
-    'support':'semirworkuaman@gmail.com',
-    'summary':'POS single category mode with click disable functionality and multiple category mode with predefined categories.',
-    'description': """
+    "category":"Point of Sale",
+    "support":"semirworkuaman@gmail.com",
+    "summary":"""
+
+POS single category mode with click disable functionality and multiple category mode with predefined categories
+
+""",
+    "description": """
 
 =======================
 
@@ -21,22 +25,22 @@ This module adds product category filter on Point of Sale:
 used with Single Category Mode
 
 """,
-    'depends': ['point_of_sale'],
+    "depends": ["point_of_sale"],
     
-    'website':'https://github.com/emanabdumalik/odoo/tree/10.0/point_of_sale_categ_filter',
-    'images': [
-        'images/main_screenshot.png',
+    "website":"https://github.com/emanabdumalik/odoo/tree/10.0/point_of_sale_categ_filter",
+    "images": [
+        "images/main_screenshot.png",
     ],    
-    'data': [
+    "data": [
         
-        'views/pos_config_views.xml',
-        'views/pos_product_category_filter_templates.xml',
+        "views/pos_config_views.xml",
+        "views/pos_product_category_filter_templates.xml",
         
     ],
-    'qweb': [
+    "qweb": [
         
-        'static/src/xml/category_filter.xml',
+        "static/src/xml/category_filter.xml",
     ],
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
