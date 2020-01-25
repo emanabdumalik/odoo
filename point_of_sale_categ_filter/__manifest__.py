4be20339bb462e3cf0378785d407384ac45e7be2
emanabdumalik <52609967+emanabdumalik@users.noreply.github.com>
2020-01-26 00:11:15 +0300

    Update __manifest__.py

--- point_of_sale_categ_filter/__manifest__.py
+++ point_of_sale_categ_filter/__manifest__.py
@@ -9,7 +9,6 @@
     "license":"LGPL-3",
     "category":"Point of Sale",
     "support":"semirworkuaman@gmail.com",
-    "summary":"POS single category mode with click disable functionality and multiple category mode with predefined categories",
     "description":"""
 
 =======================
