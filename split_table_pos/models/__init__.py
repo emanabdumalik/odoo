4ec8db2910be1f8a3b0419acbb5cd036d31015e9
Semir <semirworkuaman@gmail.com>
2020-03-11 21:26:15 +0300

    Update Solitable

--- split_table_pos/models/__init__.py
+++ split_table_pos/models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import pos_config
-import res_users
-import pos_order
+from . import pos_config
+from . import res_users
+from . import pos_order
 
 
