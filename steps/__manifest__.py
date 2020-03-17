de5b0ee72ca01be24a8d3c9bcddb04c0aeaf030e
Semir <semirworkuaman@gmail.com>
2020-03-17 17:41:00 +0300

    Updated Steps

---
+++ steps/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2019 Rimes (<http://www.naolhospital.com>)
+# License LGPL-3.0.
+{
+    "name": "Process Steps Builder",
+    "version": "1.0.0.0",
+    "author": "Eman",
+    "category": "Extra Tools",
+    "depends": [
+        "base","web","web_widget_flow_chart"
+    ],
+    "summary":"A module for creating steps for processes and work flows.",
+    
+    "data": [
+        "views/views.xml",
+    ],
+    
+    'images': ['static/description/banner.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
