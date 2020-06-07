af27604398c8c8bb94ff9c750b6a98300c05c637
Semir <semirworkuaman@gmail.com>
2020-06-07 21:07:40 +0300

    Added attendance employee picture

---
+++ attendance_employee_picture/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Employee Picture on Attendance',
+    'version': '1.0',
+    'category': 'Human Resource',
+    'sequence': 6,
+    'summary': 'Show Employee Picture on Attendance',
+    'description':"""
+        Show Employee Picture on Attendance
+    """,
+    'depends': ['base','hr_attendance'],
+    'website': 'https://www.naolhospital.com',
+    'author':'Eman',
+    'data': [
+        
+        
+        'views/assets.xml'
+        
+    ],
+    'qweb': [
+        'static/src/xml/*.xml'
+        
+    ],
+    'demo': [
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+}
