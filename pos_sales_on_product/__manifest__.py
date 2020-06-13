a8b835c151554e6831a28b108f6e2f139d856480
eman.abdumali <semirworkuaman@gmail.com>
2020-06-13 10:54:14 +0300

    Added Pos Product Widget

---
+++ pos_sales_on_product/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Pos Sales Count Status Widget on Product",
+    'version': '1.0',
+    'category': 'Point Of Sale',
+    'author': 'Eman',
+    'license': 'LGPL-3',
+    'website': 'http://naolhospital.com',
+    'sequence': 7,
+    'description': "Shows number of pos sales on product",
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/views.xml'
+    ],
+    'qweb': [
+        
+    ],
+    'demo': [],
+    'application': False,
+    'installable':True,
+    'auto_install':False,
+    'images': ['static/description/banner.png'],
+    'support': 'semirworkuaman@gmail.com'
+}
+
